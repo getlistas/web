9f53f326241c0bc2194dd9981145eee7d1ce2507
gillchristian <gillchristiang@gmail.com>
2020-12-27 23:59:48 +0100

    feat(lists): Toggle show more

--- src/Component/HTML/List.purs
+++ src/Component/HTML/List.purs
@@ -2,13 +2,14 @@ module Listasio.Component.HTML.List where
 
 import Prelude
 
-import Data.Array (head, null, snoc)
+import Data.Array (head, null, snoc, tail)
 import Data.Either (note)
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Listasio.Capability.Resource.Resource (class ManageResource, getListResources)
 import Listasio.Component.HTML.Utils (maybeElem, whenElem)
@@ -23,6 +24,7 @@ _list = SProxy :: SProxy "list"
 
 data Action
   = Initialize
+  | ToggleShowMore
 
 type Input
   = { list :: ListWithIdAndUser }
@@ -33,6 +35,7 @@ data Query a
 type State
   = { list :: ListWithIdAndUser
     , resources :: RemoteData String (Array ListResource)
+    , showMore :: Boolean
     }
 
 component :: forall o m.
@@ -49,7 +52,7 @@ component = H.mkComponent
       }
   }
   where
-  initialState { list } = { list, resources: NotAsked }
+  initialState { list } = { list, resources: NotAsked, showMore: false }
 
   handleAction :: forall slots. Action -> H.HalogenM State Action slots o m Unit
   handleAction = case _ of
@@ -59,6 +62,9 @@ component = H.mkComponent
       resources <- fromEither <$> note "Failed to load list resources" <$> getListResources list._id."$oid"
       H.modify_ _ { resources = resources }
 
+    ToggleShowMore ->
+      H.modify_ \s -> s { showMore = not s.showMore }
+
   handleQuery :: forall slots a. Query a -> H.HalogenM State Action slots o m (Maybe a)
   handleQuery = case _ of
     ResourceAdded resource a -> do
@@ -66,7 +72,7 @@ component = H.mkComponent
       pure $ Just a
 
   render :: forall slots. State -> H.ComponentHTML Action slots m
-  render { list, resources } =
+  render { list, resources, showMore } =
     HH.div
       [ HP.classes [ T.m4, T.p2, T.border4, T.borderIndigo400 ] ]
       [ HH.div [ HP.classes [ T.textLg, T.borderB2, T.borderGray200, T.mb4 ] ] [ HH.text list.title ]
@@ -80,5 +86,29 @@ component = H.mkComponent
             [ HP.classes [ T.mt4 ] ]
             [ HH.text "Next item: "
             , HH.a [ HP.classes [ T.underline ], HP.href next.url ] [ HH.text next.title ]
+            ]
+      , maybeElem (tail =<< toMaybe resources) \rest ->
+          HH.div
+            []
+            [ whenElem (not $ null rest) \_ ->
+                HH.button
+                  [ HE.onClick \_ -> Just ToggleShowMore
+                  , HP.classes
+                      [ T.cursorPointer
+                      , T.p1
+                      , T.bgGray300
+                      , T.textWhite
+                      , T.shadowMd
+                      , T.hoverBgPink700
+                      , T.focusOutlineNone
+                      , T.focusRing2
+                      , T.focusRingPurple600
+                      ]
+                  ]
+                  [ HH.text $ if showMore then "Show less" else "Show more" ]
+            , whenElem showMore \_ ->
+                HH.div
+                  [ HP.classes [ T.mt4, T.flex, T.flexCol ] ]
+                  $ map (\{ url, title } -> HH.a [ HP.classes [ T.underline ], HP.href url ] [ HH.text title ]) rest
             ]
       ]
