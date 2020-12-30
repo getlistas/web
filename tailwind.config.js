a2a0813bea3d29f846a9d8e8186c1b2e5ad1e473
gillchristian <gillchristiang@gmail.com>
2020-12-30 23:55:10 +0100

    feat(styles): Add desing system colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@ module.exports = {
   purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          400: '#565B6C',
+          300: '#7B829A',
+          200: '#A0AEC0',
+          100: '#FAF9F9',
+        },
+        kiwi: {
+          DEFAULT: '#89B0AE'
+        },
+        durazno: {
+          light: '#F8D7BE',
+          DEFAULT: '#F9B7A9'
+        },
+        manzana: {
+          DEFAULT: '#E9755B'
+        },
+      }
+    },
   },
   variants: {
     extend: {
