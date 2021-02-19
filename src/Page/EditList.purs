f96f85623330bcb749c3fbd7b3c230cf4ab1c030
gillchristian <gillchristiang@gmail.com>
2021-02-19 14:21:09 +0100

    feat(list): Delete list

--- src/Page/EditList.purs
+++ src/Page/EditList.purs
@@ -11,15 +11,16 @@ import Effect.Aff.Class (class MonadAff)
 import Formless as F
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Listasio.Capability.Navigate (class Navigate, navigate_)
-import Listasio.Capability.Resource.List (class ManageList, getListBySlug, updateList)
+import Listasio.Capability.Navigate (class Navigate, navigate, navigate_)
+import Listasio.Capability.Resource.List (class ManageList, deleteList, getListBySlug, updateList)
 import Listasio.Component.HTML.Layout as Layout
 import Listasio.Component.HTML.ListForm as ListForm
 import Listasio.Data.Lens (_list)
 import Listasio.Data.List (CreateListFields, ListWithIdAndUser)
 import Listasio.Data.Profile (ProfileWithIdAndEmail)
-import Listasio.Data.Route (Route)
+import Listasio.Data.Route (Route(..))
 import Listasio.Env (UserEnv)
 import Network.RemoteData (RemoteData(..), _Success)
 import Network.RemoteData as RemoteData
@@ -32,6 +33,7 @@ data Action
   | Receive { currentUser :: Maybe ProfileWithIdAndEmail, listSlug :: Slug }
   | HandleListForm CreateListFields
   | Navigate Route Event
+  | DeleteList
 
 type State
   = { currentUser :: Maybe ProfileWithIdAndEmail
@@ -96,6 +98,16 @@ component = Connect.component $ H.mkComponent
             Nothing ->
               void $ H.query F._formless unit $ F.injQuery $ ListForm.SetCreateStatus (Failure "Could not create list") unit
 
+    DeleteList -> do
+      mbList <- H.gets $ preview (_list <<< _Success)
+      case mbList of
+        Nothing -> pure unit
+        Just list -> do
+          -- TODO: deleteList should result `Maybe Unit` instead
+          -- TODO: show toast on success
+          deleteList list.id
+          navigate Dashboard
+
   render :: State -> H.ComponentHTML Action Slots m
   render { currentUser, list: mbList } =
     Layout.dashboard
@@ -118,8 +130,88 @@ component = Connect.component $ H.mkComponent
     content =
       case mbList of
         Success list ->
-          HH.slot F._formless unit ListForm.formComponent { list: Just list } $ Just <<< HandleListForm
+          HH.div
+            []
+            [ HH.slot F._formless unit ListForm.formComponent { list: Just list } $ Just <<< HandleListForm
+            , HH.section [ HP.classes [ T.mt12 ] ] [ dangerZone list ]
+            ]
 
+        -- TODO: better message
         Failure msg -> HH.div [ HP.classes [ T.textManzana ] ] [ HH.text msg ]
 
+        -- TODO: better message
         _ -> HH.text "Loading ..."
+
+    dangerZone :: ListWithIdAndUser -> _
+    dangerZone list =
+      HH.div
+        []
+        [ HH.h3
+            [ HP.classes [ T.textGray400, T.text2xl, T.fontBold ] ]
+            [ HH.text "Danger zone" ]
+        , HH.div
+            [ HP.classes
+                [ T.roundedMd
+                , T.border2
+                , T.borderManzana
+                , T.mt4
+                , T.p4
+                ]
+            ]
+            [ deleteListEl list ]
+        ]
+
+    deleteListEl :: ListWithIdAndUser -> _
+    deleteListEl list =
+      HH.div
+        [ HP.classes [ T.px4, T.py5, T.smP6 ] ]
+        [ HH.div
+            [ HP.classes [ T.smFlex, T.smItemsStart, T.smJustifyBetween ] ]
+            [ HH.div
+                []
+                [ HH.h3
+                    [ HP.classes [ T.textLg, T.leading6, T.fontMedium, T.textGray400 ] ]
+                    [ HH.text "Delete list" ]
+                , HH.div
+                    [ HP.classes [ T.mt2, T.maxWXl, T.textSm, T.textGray300 ] ]
+                    [ HH.p
+                        []
+                        [ HH.text "Once you delete a list, all it's resources are deleted as wall and cannot be recovered." ]
+                    ]
+                ]
+            , HH.div
+                [ HP.classes
+                    [ T.mt5
+                    , T.smMt0
+                    , T.smMl6
+                    , T.smFlexShrink0
+                    , T.smFlex
+                    , T.smItemsCenter
+                    ]
+                ]
+                [ HH.button
+                    [ HP.classes
+                        [ T.inlineFlex
+                        , T.itemsCenter
+                        , T.px4
+                        , T.py2
+                        , T.border
+                        , T.borderTransparent
+                        , T.fontMedium
+                        , T.roundedMd
+                        , T.textWhite
+                        , T.bgManzana
+                        , T.hoverBgOpacity75
+                        , T.focusOutlineNone
+                        , T.focusRing2
+                        , T.focusRingOffset2
+                        , T.focusRingManzana
+                        , T.smTextSm
+                        ]
+                    , HP.type_ HP.ButtonButton
+                    , HE.onClick \_ -> Just DeleteList
+                    ]
+                    [ HH.text "Delete list" ]
+                ]
+            ]
+        ]
