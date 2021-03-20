3bd2e16c55db66e90b1ec9f1fc49cf08f6780f35
gillchristian <gillchristiang@gmail.com>
2021-03-20 23:52:32 +0100

    support(api): heroku -> listas endpoint

--- env/prod/ConfigProvider.purs
+++ env/prod/ConfigProvider.purs
@@ -3,7 +3,7 @@ module ConfigProvider where
 import Listasio.Api.Request (BaseURL(..))
 
 provide :: BaseURL
-provide = BaseURL "https://doneq.herokuapp.com"
+provide = BaseURL "https://api.listas.io"
 
 splitbeeUrl :: String
 splitbeeUrl = "https://analytics.listas.workers.dev/sb.js"
