39f451d440e40b666facd14f23c5d1c6990cf649
gillchristian <gillchristiang@gmail.com>
2021-05-28 17:24:49 +0200

    fix(router): Not found rendering recursive layout
    Also, don't redirect home on Not Found

---
+++ src/Component/HTML/NotFound.purs
@@ -0,0 +1,13 @@
+module Listasio.Component.HTML.NotFound where
+
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+import Tailwind as T
+
+elem :: forall i p. HH.HTML i p
+elem =
+  HH.div
+    [ HP.classes [ T.textGray400,  T.fontMono, T.mt10, T.textCenter ] ]
+    [ HH.p [ HP.classes [ T.mb6, T.text2xl ] ] [ HH.text "404 - Not found" ]
+    , HH.p [ HP.classes [ T.text4xl ] ] [ HH.text "¯\\_(ツ)_/¯" ]
+    ]
