b4f31c6d4bfcc9c802bb2e69d258043b9a5c28a2
gillchristian <gillchristiang@gmail.com>
2021-10-31 17:16:43 +0100

    support(home): Screenshot
    Small improvements on build and manifest as well

--- scripts/fix-dist.sh
+++ scripts/fix-dist.sh
@@ -1,2 +1,2 @@
-sed 's/src="index/src="\/index/' -i dist/index.html
-sed 's/href="index/href="\/index/' -i dist/index.html
+sed 's|src="index|src="/index|g' -i dist/index.html
+sed 's|href="index|href="/index|g' -i dist/index.html
